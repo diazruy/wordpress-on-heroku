b3272289058933b1cd11f222733cf404ea4c24bb
Ruy Diaz <diazruy@gmail.com>
2014-01-11 18:34:36 -0800

    Remove ads from theme

--- config/public/wp-content/themes/ocean/sidebar.php
+++ config/public/wp-content/themes/ocean/sidebar.php
@@ -1,27 +1,16 @@
-		</div>
-		<div id="subcontent">
-			<h5>Pages</h5>
-			<ul>
-				<?php wp_list_pages('title_li='); ?>
-			</ul><br>
-			<h5>Categories</h5>
-			<ul>
-				<?php wp_list_categories('title_li='); ?>
-			</ul>
-			<br>
-			<h5>Archives</h5>
-			<ul>
-				<?php wp_get_archives('type=monthly'); ?>
-			</ul><br>
-<div style="width:160px;height:600px;margin:0 auto;"><script type="text/javascript"><!--
-google_ad_client = "pub-4634728022964105";
-/* 160x600, created 7/31/08 */
-google_ad_slot = "2440199240";
-google_ad_width = 160;
-google_ad_height = 600;
-//-->
-</script>
-<script type="text/javascript"
-src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-</script></div>
-		</div>
+    </div>
+    <div id="subcontent">
+      <h5>Pages</h5>
+      <ul>
+        <?php wp_list_pages('title_li='); ?>
+      </ul><br>
+      <h5>Categories</h5>
+      <ul>
+        <?php wp_list_categories('title_li='); ?>
+      </ul>
+      <br>
+      <h5>Archives</h5>
+      <ul>
+        <?php wp_get_archives('type=monthly'); ?>
+      </ul><br>
+    </div>
